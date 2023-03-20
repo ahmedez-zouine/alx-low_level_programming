d87365b5a51a454c026ab03fab54b9d6e098436d
azouine <myemail@gmail.com>
2023-03-20 12:48:47 -0700

    This New Fille

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 		putchar(alpha);
-	putchat("\n");
+	putchar("\n");
 	for (alpha = 'A'; alpha <= 'Z'; alpha++)
 		putchar(alpha);
 	return (0);
