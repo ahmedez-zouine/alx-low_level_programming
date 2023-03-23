03f48efeb850dc4e0b458b304148ef72fafc8dad
azouine <myemail@gmail.com>
2023-03-23 15:06:15 -0700

    remove main

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,6 +1,13 @@
 #include "main.h"
 
-int mul(int a, int b){
-	return (a*b);
+/**
+ * mul - multiply 2 integers
+ * @a: int type number
+ * @b: int type number
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
 }
 
