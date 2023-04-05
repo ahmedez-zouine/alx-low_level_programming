1e3c9b70c88565f20553155c60eb49a124edfff4
azouine <myemail@gmail.com>
2023-04-05 09:58:46 -0700

    new file updute

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,11 @@
-#include "mian.h"
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte.
+ * @s: source string
+ * @b: the contant byte for filling
+ * @n: lenght of buffer
+ * Return: new string.
+ */
 
 void _puts_recursion(char *s){
 	puts(s);
