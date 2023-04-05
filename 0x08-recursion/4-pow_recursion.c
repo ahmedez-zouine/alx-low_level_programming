01314b12d3d158f39417d43175279ec96ff2e63d
azouine <myemail@gmail.com>
2023-04-05 15:41:43 -0700

    new remove operation plus

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,8 +3,7 @@
 /**
  * _pow_recursion - returns the value of x raised to the power of y
  * @x: value to raise
- * @y: power
- *
+ * @y: powe
  * Return: result of the power
  */
 int _pow_recursion(int x, int y)
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,9 +5,9 @@ int actual_prime(int n, int i);
 /**
  * is_prime_number - says if an integer is a prime number or not
  * @n: number to evaluate
- *
  * Return: 1 if n is a prime number, 0 if not
  */
+
 int is_prime_number(int n)
 {
 	if (n <= 1)
