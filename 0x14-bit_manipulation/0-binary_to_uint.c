2239e390a617a3c8f9bb1904b7a73e6e0fa669c5
azouine <azouine@gmail.com>
2023-11-23 12:18:21 -0800

    Done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,19 +10,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int	result;
-	int	i;
+	int i;
+	unsigned int dec_val = 0;
 
-	i = 0;
-	result = 0;
 	if (!b)
 		return (0);
-	while (b[i])
+
+	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		result = 2 * result + (b[i] - '0');
-		i++;
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (result);
+
+	return (dec_val);
 }
