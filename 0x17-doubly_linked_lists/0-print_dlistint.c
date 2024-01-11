d55ee80de07a65157c99d4ea32bce47f10bb8904
Azouine <Azouine@gmail.com>
2024-01-11 13:33:52 -0800

    Done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 #include <stddef.h>
 #include <stdio.h>
+
 /**
- *
- * print_dlistint - print a double linked list
- * @h: head of the link list
- *
- * @Return: size_t, size of the linked list
- */
+  * print_dlistint - print a double linked list
+  * @h: head of the linked list
+  *
+  * Return: size_t, size of the linked list
+  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
