e2655634446bb75895de7a77e90be917f3a4dc4a
root <azouine@gmail.com>
2024-04-12 09:33:47 -0700

    Done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,23 +1,30 @@
 #include "hash_tables.h"
+#include <stdlib.h>
 
 /**
- * hash_table_create - function that creates a hash table
- * @size: the size of the hash table
- * Return: the hash table
- */
-
+* hash_table_create - creates a hash table.
+* @size: size of the array.
+*
+* Return: poiter to the nrew array or
+*          Null if it fails/error occurs.
+*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = NULL;
+	hash_table_t *meza;
 
-	if (size == 0)
+	/*Alloc mem for meza*/
+	meza = malloc(sizeof(hash_table_t));
+	if (meza == NULL)
 		return (NULL);
-	table = malloc(sizeof(hash_table_t));
-	if (!table)
+	/*Alloc mem for meza->array and Init it and its element to NULL*/
+	meza->array = calloc(size, sizeof(hash_table_t *));
+	if (meza->array == NULL)
+	{
+		free(meza);
 		return (NULL);
-	table->size = size;
-	table->array = malloc(sizeof(hash_node_t *) * size);
-	if (!table->array)
-		return (NULL);
-	return (table);
+	}
+
+	meza->size = size;
+
+	return (meza);
 }
